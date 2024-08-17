3fe5509787f1c622e0e28f1572a2681da6f08097
aledenro <133998613+aledenro@users.noreply.github.com>
2024-08-17 17:30:21 -0600

    fix inserts

--- sql-scripts/table_inserts.sql
+++ sql-scripts/table_inserts.sql
@@ -580,13 +580,19 @@ VALUES
 ('Luis', 'Martínez Herrera', 'luis.martinez@example.com', '555-1357', FALSE, '$2y$10$4ibFmNkT0J89tgDNYKQ7VeH07bYTT02Anqt1fN1lRLgZTlXI7Wa6O', 'usuario'),
 ('Admin', 'Admin', 'admin@gmail.com', '1111-1111', FALSE, '$2y$10$4ibFmNkT0J89tgDNYKQ7VeH07bYTT02Anqt1fN1lRLgZTlXI7Wa6O', 'admin');
 
+INSERT INTO Categoria_Anuncio (nombre) VALUES
+('Servicios Públicos'),
+('Mascotas Perdidas y Encontradas'),
+('Eventos Comunitarios'),
+('Seguridad y Prevención'),
+('Comercio y Ofertas');
+
 -- test inserts anuncios 
 INSERT INTO Anuncio (id_Usuario, Titulo, Estado, Descripcion, Fecha, oficial, url_imagen, id_Provincia, id_Canton, id_Distrito) 
 VALUES 
-(1, 'Perro perdido en el parque central', TRUE, 'Se perdió un perro de raza Labrador, color amarillo, responde al nombre de Max. Se perdió en el parque central el 1 de agosto. Por favor, si lo encuentran, contactarse con nosotros.', '2024-08-01', FALSE, 'https://firebasestorage.googleapis.com/v0/b/compliants-anouncements-web.appspot.com/o/imgs%2Flabrador.jpg?alt=media&token=8d7c2c82-9c4e-44c1-b10e-9020012a0e8b', 1, 101, 10101),
-(2, 'Aviso de corte de agua', TRUE, 'Se informa a los vecinos que habrá un corte de agua el próximo lunes 5 de agosto desde las 8:00 AM hasta las 2:00 PM por mantenimiento. Disculpen las molestias.', '2024-08-02', TRUE, 'https://firebasestorage.googleapis.com/v0/b/compliants-anouncements-web.appspot.com/o/imgs%2FcorteAgua.jpg?alt=media&token=1c3905c6-ebe9-4cb3-81d9-7db2af6649f6', 1, 102, 10201),
-(3, 'Venta de garaje', TRUE, 'Estamos organizando una venta de garaje este sábado 3 de agosto a partir de las 10:00 AM en la calle 123. Habrá muebles, ropa, y más a precios económicos. ¡No se lo pierdan!', '2024-08-02', FALSE, '', 1, 103, 10301);
-
+(1, 'Perro perdido en el parque central', TRUE, 'Se perdió un perro de raza Labrador, color amarillo, responde al nombre de Max. Se perdió en el parque central el 1 de agosto. Por favor, si lo encuentran, contactarse con nosotros.', '2024-08-01', FALSE, 'https://firebasestorage.googleapis.com/v0/b/compliants-anouncements-web.appspot.com/o/imgs%2Flabrador.jpg?alt=media&token=8d7c2c82-9c4e-44c1-b10e-9020012a0e8b', 1, 101, 10101, 2), -- Mascotas Perdidas y Encontradas
+(2, 'Aviso de corte de agua', TRUE, 'Se informa a los vecinos que habrá un corte de agua el próximo lunes 5 de agosto desde las 8:00 AM hasta las 2:00 PM por mantenimiento. Disculpen las molestias.', '2024-08-02', TRUE, 'https://firebasestorage.googleapis.com/v0/b/compliants-anouncements-web.appspot.com/o/imgs%2FcorteAgua.jpg?alt=media&token=1c3905c6-ebe9-4cb3-81d9-7db2af6649f6', 1, 102, 10201, 1), -- Servicios Públicos
+(3, 'Venta de garaje', TRUE, 'Estamos organizando una venta de garaje este sábado 3 de agosto a partir de las 10:00 AM en la calle 123. Habrá muebles, ropa, y más a precios económicos. ¡No se lo pierdan!', '2024-08-02', FALSE, '', 1, 103, 10301, 5); -- Comercio y Ofertas
 
 INSERT INTO Categoria_Denuncia (nombre)
 VALUES
