6912c6cadf5ee65c62efe5674236c8498a501f64
aledenro <133998613+aledenro@users.noreply.github.com>
2024-08-18 18:08:56 -0600

    Revert "sistema de busqueda en php y js, se agregó el procedimiento almacenado a la base de datos para completar la busqueda"
    This reverts commit 104d3840283fb26cdd97cc147847d42e92e76667.

--- js/verifyIfLoggedIn.js
+++
@@ -1,7 +0,0 @@
-function verifyIfLoggedIn() {
-  return fetch("/php/verifyIfLoggedIn.php")
-    .then((response) => response.json())
-    .then((data) => {
-      return data;
-    });
-}
--- php/verifyIfLoggedIn.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if (!isset($_SESSION["id_Usuario"])){
-        echo json_encode(array("loggedIn" => false));
-    } else{
-        echo json_encode(array("loggedIn" => true, "id_Usuario" => $_SESSION["id_Usuario"], "nombre" => $_SESSION["Nombre"], "apellidos" => $_SESSION["Apellidos"], "rol" => $_SESSION["rol"]));
-    }
-
-?>
